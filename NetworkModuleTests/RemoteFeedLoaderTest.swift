725e3a510a1c6cce269905004dbbd4126b1ce717
Sam <klioop2@gmail.com>
2022-02-08 22:46:25 +0900

    Delivers empty items array on 200 HTTP response with JSON empty list

--- NetworkModuleTests/RemoteFeedLoaderTest.swift
+++ NetworkModuleTests/RemoteFeedLoaderTest.swift
@@ -69,6 +69,18 @@ class RemoteFeedLoaderTest: XCTestCase {
         })
     }
     
+    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
+        let (sut, client) = makeSUT()
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load { capturedResults.append($0) }
+        
+        let emptyJSON = Data("{\"items\": []}".utf8)
+        client.complete(withStatusCode: 200, data: emptyJSON)
+        
+        XCTAssertEqual(capturedResults, [.success([])])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
