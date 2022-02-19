48c11608c18f234d434a7a9f7bc7ee3c921d9bcc
Sam <klioop2@gmail.com>
2022-02-20 01:02:50 +0900

    Save command succeeds on successful cache insertion

--- NetworkModuleTests/Feed Cache/LocalFeedLoaderTest.swift
+++ NetworkModuleTests/Feed Cache/LocalFeedLoaderTest.swift
@@ -65,6 +65,10 @@ class FeedStore {
         insertionCompletions[index](error)
     }
     
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+    
     
 }
 
@@ -143,6 +147,24 @@ class LocalFeedLoaderTest: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
         XCTAssertEqual(receivedError as NSError?, insertionError)
+    }
+    
+    func test_save_succeedsOnSuccessfulCacheInsertion() {
+        let items = [uniuqeItem(), uniuqeItem()]
+        let (sut, store) =  makeSUT()
+        let exp = expectation(description: "Wait for save completion")
+        
+        var receivedError: Error?
+        sut.save(items) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertNil(receivedError)
     }
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
