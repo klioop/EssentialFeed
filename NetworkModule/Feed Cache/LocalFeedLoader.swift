38f30f31772dd216870f6c271eb73711c46f1aaf
Sam <klioop2@gmail.com>
2022-02-25 23:01:22 +0900

    Move `FeedCachePolicy` to its own file

--- NetworkModule/Feed Cache/LocalFeedLoader.swift
+++ NetworkModule/Feed Cache/LocalFeedLoader.swift
@@ -7,25 +7,7 @@
 
 import Foundation
 
-// You can never have an instance of a FeedCachePolicy because it needs no identity and it holds no state
-private final class FeedCachePolicy {
-    private init() {}
-    
-    private static let calendar = Calendar(identifier: .gregorian)
-    
-    private static var maxCacheAgeInDays: Int {
-        return 7
-    }
-    
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
-        
-        return date < maxCacheAge
-    }
-}
-
 public final class LocalFeedLoader {
-    
     private let store: FeedStore
     private let currentDate: () -> Date
     
