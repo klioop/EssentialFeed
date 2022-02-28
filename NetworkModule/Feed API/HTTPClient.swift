4be1a93d6d8380485c19ef587c033db41bb9ebb3
Sam <klioop2@gmail.com>
2022-02-28 20:47:16 +0900

    Add comments to document that completion handlers in any `HTTPClient` can be invoked in any thread. Clients are responsible to dispatch to appropriate threads, if needed.

--- NetworkModule/Feed API/HTTPClient.swift
+++ NetworkModule/Feed API/HTTPClient.swift
@@ -15,5 +15,7 @@ public enum HTTPClientResult {
 // extension URLSession, or AF is possible
 public protocol HTTPClient {
     // error type should not be RemoteFeedLoader.Error. It comes from the domain in HTTP
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
