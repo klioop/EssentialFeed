3cc5245c2c28cfa8ec16b503f9da044ad6de3a99
Sam <klioop2@gmail.com>
2022-03-06 18:04:58 +0900

    Add `NetworkModuleCacheIntegrationTests` target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ NetworkModuleCacheIntegrationTests/NetworkModuleCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkModuleCacheIntegrationTests.swift
+//  NetworkModuleCacheIntegrationTests
+//
+//  Created by klioop on 2022/03/06.
+//
+
+import XCTest
+
+class NetworkModuleCacheIntegrationTests: XCTestCase {
+
+}
