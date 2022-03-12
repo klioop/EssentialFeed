1827a7a684a77ed4a8d351ff425c7207e3259ba9
Sam <klioop2@gmail.com>
2022-03-12 13:24:15 +0900

    Loads feed on view did load

--- NetworkModule/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ NetworkModule/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -6,12 +6,20 @@
 //
 
 import XCTest
+import UIKit
 
-class FeedViewController {
-    let loader: FeedViewControllerTests.FeedLoaderSpy
+class FeedViewController: UIViewController {
+    var loader: FeedViewControllerTests.FeedLoaderSpy?
     
-    init(loader: FeedViewControllerTests.FeedLoaderSpy) {
+    convenience init(loader: FeedViewControllerTests.FeedLoaderSpy) {
+        self.init()
         self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loader?.load()
     }
 }
 
@@ -24,7 +32,20 @@ class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
+    func test_viewDidLoad_loadsFeed() {
+        let loader = FeedLoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
     class FeedLoaderSpy {
-        var loadCallCount = 0
+        private(set) var loadCallCount = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
