06a3cce65638ff14d58133d3b5ea718fdf7eebad
Sam <klioop2@gmail.com>
2022-03-20 19:38:30 +0900

    Move `FeedLoadingViewModel` and `FeedViewModel` to its own files

---
+++ NetworkModule/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by klioop on 2022/03/20.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ NetworkModule/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by klioop on 2022/03/20.
+//
+
+import NetworkModule
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
