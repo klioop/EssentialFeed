b131d298b9d2e26a56596133031b5b7a3f928eb3
Sam <klioop2@gmail.com>
2022-04-03 22:43:16 +0900

    Reusable table view cells from storyboard configuration

--- NetworkModule/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ NetworkModule/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -17,7 +17,6 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     public override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.register(FeedImageCell.self)
         refreshController?.refresh()
     }
     
