57a183359c9c03c79912213c9ff5bc0b40f8fc5e
Sam <klioop2@gmail.com>
2022-04-06 20:10:32 +0900

    Move `FeedLoadingViewModel` to its own file

---
+++ NetworkModule/NetworkModule/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingView.swift
+//  NetworkModule
+//
+//  Created by klioop on 2022/04/06.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
