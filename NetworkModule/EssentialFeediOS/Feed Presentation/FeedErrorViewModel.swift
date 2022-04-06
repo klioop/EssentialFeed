11db5cfff6f7b401d61ec1fe654cbc7f7a4a0f59
Sam <klioop2@gmail.com>
2022-04-06 18:24:53 +0900

    Move `FeedErrorViewModel` to its own file

---
+++ NetworkModule/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by klioop on 2022/04/06.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
