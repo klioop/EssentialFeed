5b6e462ee25619ebac64ee14a81f467718e4d592
Sam <klioop2@gmail.com>
2022-04-11 23:48:55 +0900

    `LocalFeedImageDataLoader.loadImageData` delivers not found error when store can't find image data for url

--- NetworkModule/NetworkModuleTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ NetworkModule/NetworkModuleTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -22,6 +22,7 @@ final class LocalFeedImageDataLoader: FeedImageDataLoader {
     
     public enum Error: Swift.Error {
         case failed
+        case notFound
     }
     
     private let store: FeedImageDataStore
@@ -32,7 +33,9 @@ final class LocalFeedImageDataLoader: FeedImageDataLoader {
     
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         store.retrieve(dataForURL: url) { result in
-            completion(.failure(Error.failed))
+            completion(result
+                        .mapError { _ in Error.failed }
+                        .flatMap { _ in .failure(Error.notFound) })
         }
         return Task()
     }
@@ -64,6 +67,14 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         })
     }
     
+    func test_loadImageDataFromURL_deliversNotFoundErrorOnNotFound() {
+        let (sut, store) = makeSUT()
+        
+        expect(sut, toCompleteWith: notFound(), when: {
+            store.complete(with: .none)
+        })
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
@@ -76,6 +87,10 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
     
     private func failure() -> FeedImageDataLoader.Result {
         .failure(LocalFeedImageDataLoader.Error.failed)
+    }
+    
+    private func notFound() -> FeedImageDataLoader.Result {
+        .failure(LocalFeedImageDataLoader.Error.notFound)
     }
     
     private func expect(_ sut: LocalFeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
@@ -115,6 +130,10 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         
         func complete(with error: Error, at index: Int = 0) {
             completions[index](.failure(error))
+        }
+        
+        func complete(with data: Data?, at index: Int = 0) {
+            completions[index](.success(data))
         }
     }
 }
