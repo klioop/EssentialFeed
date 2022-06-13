8298699dd69752d5244348ecbd99ba3a0a5a66fc
Sam <klioop2@gmail.com>
2022-06-13 16:21:12 +0900

    delete unnecessary lines

--- NetworkModule/NetworkModule/Feed Cache/LocalFeedLoader.swift
+++ NetworkModule/NetworkModule/Feed Cache/LocalFeedLoader.swift
@@ -99,5 +99,3 @@ private extension Array where Element == LocalFeedImage {
         return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     }
 }
-
-
