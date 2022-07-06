5eea135d09238de584ed0b76b74e043a5caddd45
Sam <klioop2@gmail.com>
2022-07-06 21:35:21 +0900

    Execute RunLoop to prevent memory leaks in the test

--- EssentialApp/EssentialAppTests/Helpers/FeedViewControllerTests+Helpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewControllerTests+Helpers.swift
@@ -20,6 +20,8 @@ extension FeedUIIntegrationTests {
         feed.enumerated().forEach { index, image in
             assertThat(sut, hasConfigureFor: image, at: index, file: file, line: line)
         }
+        
+        executeRunLoopToCleanReferences()
     }
 
     private func assertThat(_ sut: FeedViewController, hasConfigureFor image :FeedImage, at index: Int, file: StaticString = #filePath, line: UInt = #line) {
@@ -35,5 +37,9 @@ extension FeedUIIntegrationTests {
         XCTAssertEqual(cell.locationText, image.location, "Expected location text to be \(String(describing: image.location)) for image view at index \(index)", file: file, line: line)
         
         XCTAssertEqual(cell.descriptionText, image.description, "Expected description text to be \(String(describing: image.description)) for image view at index \(index)", file: file, line: line)
+    }
+    
+    private func executeRunLoopToCleanReferences() {
+        RunLoop.current.run(until: Date())
     }
 }
