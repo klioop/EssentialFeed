f8f560a4d499ec6bc9e0533b368ed46f5c9e92c6
Sam <klioop2@gmail.com>
2022-07-18 18:15:31 +0900

    Remove FeedLoader protocol: No more strategy pattern is needed

--- EssentialApp/EssentialAppTests/Helpers/FeedLoaderSpy+FeedImageDataLoaderSpy.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedLoaderSpy+FeedImageDataLoaderSpy.swift
@@ -6,27 +6,30 @@
 //
 
 import Foundation
+import Combine
 import NetworkModule
 import EssentialFeediOS
 
-class FeedLoaderSpy: FeedLoader, FeedImageDataLoader {
-    private(set) var feedRequests = [(FeedLoader.Result) -> Void]()
+class FeedLoaderSpy: FeedImageDataLoader {
+    private(set) var feedRequests = [PassthroughSubject<[FeedImage], Error>]()
                     
     var loadFeedCallCount: Int {
         feedRequests.count
     }
     
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        feedRequests.append(completion)
+    func loadPublisher() -> AnyPublisher<[FeedImage], Error> {
+        let publisher = PassthroughSubject<[FeedImage], Error>()
+        feedRequests.append(publisher)
+        return publisher.eraseToAnyPublisher()
     }
     
     func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
-        feedRequests[index](.success(feed))
+        feedRequests[index].send(feed)
     }
     
     func completeFeedLoadingWithError(at index: Int = 0) {
         let error = NSError(domain: "an error", code: 0)
-        feedRequests[index](.failure(error))
+        feedRequests[index].send(completion: .failure(error))
     }
     
     // MARK: - FeedImageDataLoader
--- NetworkModule/NetworkModuleEndToEndTests/NetworkModuleEndToEndTests.swift
+++ NetworkModule/NetworkModuleEndToEndTests/NetworkModuleEndToEndTests.swift
@@ -44,12 +44,12 @@ class NetworkModuleEndToEndTests: XCTestCase {
         }
     }
     
-    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> FeedLoader.Result? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> Swift.Result<[FeedImage], Error>? {
         let client = ephemeralClient()
         
         let exp = expectation(description: "Wait for completion")
         
-        var receivedResult: FeedLoader.Result?
+        var receivedResult: Swift.Result<[FeedImage], Error>?
         client.get(from: feedTestServerURL) { result in
             receivedResult = result.flatMap { data, response in
                 do {
