dbceb4852a80b1bdbb9c37909b8e012bc82a2042
Sam <klioop2@gmail.com>
2022-07-19 20:43:28 +0900

    Duplicate FeedPresenter as LoadResourcePresenter

---
+++ NetworkModule/NetworkModule/Shared Presentation/LoadResourcePresenter.swift
@@ -0,0 +1,49 @@
+//
+//  LoadResourcePresenter.swift
+//  NetworkModule
+//
+//  Created by klioop on 2022/07/19.
+//
+
+import Foundation
+
+public final class LoadResourcePresenter {
+    private let feedView: FeedView
+    private let loadingView: FeedLoadingView
+    private let errorView: FeedErrorView
+    
+    public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
+        self.feedView = feedView
+        self.loadingView = loadingView
+        self.errorView = errorView
+    }
+    
+    public var errorMessage: String {
+        NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
+                          tableName: "Feed",
+                          bundle: Bundle(for: FeedPresenter.self),
+                          comment: "Error message displayed when we can't load feed from the server")
+    }
+    
+    public static var title: String {
+        NSLocalizedString("FEED_VIEW_TITLE",
+                          tableName: "Feed",
+                          bundle: Bundle(for: FeedPresenter.self),
+                          comment: "Title for the feed view")
+    }
+    
+    public func didStartLoadingFeed() {
+        errorView.display(.noError)
+        loadingView.display(.init(isLoading: true))
+    }
+    
+    public func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+    
+    public func didFinishLoadingFeed(with error: Error) {
+        errorView.display(FeedErrorViewModel.error(message: errorMessage))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+}
---
+++ NetworkModule/NetworkModuleTests/Shared Presentation /LoadResourcePresenterTests.swift
@@ -0,0 +1,98 @@
+//
+//  LoadResourcePresenterTests.swift
+//  NetworkModuleTests
+//
+//  Created by klioop on 2022/07/19.
+//
+
+import XCTest
+import NetworkModule
+
+class LoadResourcePresenterTests: XCTestCase {
+    
+    func test_title_isLocalized() {
+        XCTAssertEqual(LoadResourcePresenter.title, localized("FEED_VIEW_TITLE"))
+    }
+    
+    func test_init_doesNotSendMessage() {
+        let (_, view) = makeSUT()
+        
+        XCTAssertTrue(view.messages.isEmpty)
+    }
+    
+    func test_didStartLoadingFeed_displaysNoErrorMessageAndStartsLoading() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [
+            .display(message: .none),
+            .display(isLoading: true)
+        ])
+    }
+    
+    func test_didFinishLoadingFeed_displaysFeedAndStopsLoading() {
+        let (sut, view) = makeSUT()
+        let feed = uniqueImageFeed().models
+        
+        sut.didFinishLoadingFeed(with: feed)
+        
+        XCTAssertEqual(view.messages, [
+            .display(feed: feed),
+            .display(isLoading: false)
+        ])
+    }
+    
+    func test_didFinishLoadingFeedWithError_displaysLocalizedErrorMessageAndStopsLoading() {
+        let (sut, view) = makeSUT()
+        
+        sut.didFinishLoadingFeed(with: anyNSError())
+        
+        XCTAssertEqual(view.messages, [
+            .display(message: localized("FEED_VIEW_CONNECTION_ERROR")),
+            .display(isLoading: false)
+        ])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LoadResourcePresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = LoadResourcePresenter(feedView: view, loadingView: view, errorView: view)
+        trackMemoryLeak(view, file: file, line: line)
+        trackMemoryLeak(sut, file: file, line: line)
+        return (sut, view)
+    }
+    
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: LoadResourcePresenter.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+    
+    private class ViewSpy: FeedErrorView, FeedLoadingView, FeedView {
+        enum Message: Hashable {
+            case display(message: String?)
+            case display(isLoading: Bool)
+            case display(feed: [FeedImage])
+        }
+        
+        private(set) var messages = Set<Message>()
+        
+        func display(_ viewModel: FeedViewModel) {
+            messages.insert(.display(feed: viewModel.feed))
+        }
+        
+        func display(_ viewModel: FeedLoadingViewModel) {
+            messages.insert(.display(isLoading: viewModel.isLoading))
+        }
+        
+        func display(_ viewModel: FeedErrorViewModel) {
+            messages.insert(.display(message: viewModel.message))
+        }
+    }
+}
