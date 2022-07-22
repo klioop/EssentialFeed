82c4c138019ca3f5438274cd82b031f686120485
Sam <klioop2@gmail.com>
2022-07-22 16:47:28 +0900

    Make `LoadResourcePresenter` generic over the `Resource` type

--- NetworkModule/NetworkModuleTests/Shared Presentation /LoadResourcePresenterTests.swift
+++ NetworkModule/NetworkModuleTests/Shared Presentation /LoadResourcePresenterTests.swift
@@ -51,13 +51,14 @@ class LoadResourcePresenterTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    private typealias SUT = LoadResourcePresenter<String, ViewSpy>
     
     private func makeSUT(
-        mapper: @escaping LoadResourcePresenter.Mapper = { _ in "any"},
+        mapper: @escaping SUT.Mapper = { _ in "any" },
         file: StaticString = #filePath,
-        line: UInt = #line) -> (sut: LoadResourcePresenter, view: ViewSpy) {
+        line: UInt = #line) -> (sut: SUT, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = LoadResourcePresenter(resourceView: view, loadingView: view, errorView: view, mapper: mapper)
+        let sut = SUT(resourceView: view, loadingView: view, errorView: view, mapper: mapper)
         trackMemoryLeak(view, file: file, line: line)
         trackMemoryLeak(sut, file: file, line: line)
         return (sut, view)
@@ -65,7 +66,7 @@ class LoadResourcePresenterTests: XCTestCase {
     
     private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
         let table = "Feed"
-        let bundle = Bundle(for: LoadResourcePresenter.self)
+        let bundle = Bundle(for: SUT.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
             XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
@@ -74,6 +75,8 @@ class LoadResourcePresenterTests: XCTestCase {
     }
     
     private class ViewSpy: ResourceView, FeedErrorView, FeedLoadingView {
+        typealias ResourceViewModel = String
+        
         enum Message: Hashable {
             case display(message: String?)
             case display(isLoading: Bool)
